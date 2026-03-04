5ce2c03e864264e2cf7684dde73133ebeb85d017
FutureNN | Jan <168673668+FutureJan@users.noreply.github.com>
2026-03-04 20:27:50 +0100

    3-Create Game Loop - Add Linux window class

---
+++ GameEngineLib/src/Window/Linux/WindowLinux.cpp
@@ -0,0 +1,17 @@
+﻿#include "WindowLinux.h"
+
+WindowLinux::WindowLinux(const WindowConfig & config, int showCmd) {
+
+}
+
+WindowLinux::~WindowLinux() {
+
+}
+
+void WindowLinux::show(const int showCmd) {
+
+}
+
+bool WindowLinux::processMessages() {
+    return true;
+}
---
+++ GameEngineLib/src/Window/Linux/WindowLinux.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Window/IWindow.h"
+
+class WindowLinux : public IWindow{
+public:
+    WindowLinux(const WindowConfig & config, int showCmd);
+
+    ~WindowLinux() override;
+
+    WindowLinux(const WindowLinux&) = delete;
+    WindowLinux(WindowLinux&&) = delete;
+    WindowLinux& operator=(const WindowLinux&) = delete;
+    WindowLinux& operator=(WindowLinux&&) = delete;
+
+    void show(int showCmd) override;
+    bool processMessages() override;
+};
